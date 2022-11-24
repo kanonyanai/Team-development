dd7cc9fd8d444c02123da3e17539bef3b3fc6827
te190728 <tejc190728@gmail.com>
2022-11-24 15:07:18 +0900

    練習

--- samplelambda.py
+++ samplelambda.py
@@ -7,3 +7,9 @@ def samplelambda (event, context):
  #defの後は関数の名前(任意)　event引数＝関数に渡される値　context引数＝実行環境の情報
  x = int(event["x"]) #渡された値の取得
  y = int(event["y"])
+
+ print("x=" + str(x)) #print関数で表示する。strかんすうで数値を文字列に
+ print("y=" + str(y))
+
+ answer = {"result" : x/y}
+ return json.dumps(answer) #答えを「json.dumps()」でjson形式に変換したものを表示する
