edf01f7884d64ad2ada214677ab5b3c62e1c300c
te190806 <190806@jc-21.jp>
2023-01-12 16:58:24 +0900

    一覧表示のand演算の調整中（現段階だと選択出来るのは1つだけ）
    Signed-off-by: te190806 <190806@jc-21.jp>

--- home/and.js
+++ home/and.js
@@ -1,26 +1,21 @@
-$(function (){
-    var $chkbxFilter_all = $('#all');
-
-    
-    //チェックボックスがクリックされた時の動作
-    $("#select colTitle label input").click(function() {
-        
-        $(this).parent().toggleClass("selected");
-        
-        $.each($chkbxFilter_tags, function() {
-            if($('#' + this).is(':checked')) {
-                         $("#result " + $chkbxFilter_blocks + ":not(." + this + ")").addClass('hidden-not-' + this);
-                         $chkbxFilter_all.prop('checked',false).parent().removeClass("selected");
-                }
-            else if($('#' + this).not(':checked')) {
-                         $("#result " + $chkbxFilter_blocks + ":not(." + this + ")").removeClass('hidden-not-' + this);
-                }
-        });
-        
-        //チェックボックスが1つも選択されていない場合に、絞り込まないボタンにclass="selected"をつける
-        if ($('.sort:checked').length == 0 ){
-            $chkbxFilter_all.prop('checked',true).parent().addClass("selected");
-            $(".sort").parent().removeClass("selected");
+$(function() {
+    var lists = $('.result tr');
+    $(document).on('change', '.select colTitle th categori', function() {
+        lists.show();
+        for (var i = 0; i < $('.select colTitle th categori').length; i++) {
+            // 絞り込みの項目を取得
+            var item = $('.select colTitle th categori').eq(i).attr('name');
+            // 絞り込みの対象を取得
+            var target = $('.select colTitle th categori').eq(i).val();
+ 
+            if(target != '') {
+                for (var j = 0; j < lists.length; j++) {
+                    // 絞り込み対象でない場合は非表示
+                    if(!lists.eq(j).find('.' + item).find('tr').hasClass(target)) {
+                        lists.eq(j).hide();
+                    }
+                };
+            }
         };
-	});     
+    });
 });
