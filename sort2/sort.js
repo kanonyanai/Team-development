c877985e60a414c825dded3561ee1f6a5ba03511
te190728 <tejc190728@gmail.com>
2023-01-23 12:05:33 +0900

    更新

--- sort2/sort.js
+++ sort2/sort.js
@@ -1,31 +1,46 @@
-$(function (){
-    var $chkbxFilter_all = $('#all');
+var searchBox = '.search-box';//1回目と二回目
+var searchItem = '.search-box input';
+//var searchItem2 = '.search-box2 input';
+var listItem = '.cat_image';  //えらばれるやつ
+var hideClass = 'is-hide';
 
-    //絞り込まないボタンをクリック時にチェックボックスをオフにする
-    $chkbxFilter_all.click(function() {
-		  $(".sort").prop('checked',false);
-		  $chkbxFilter_all.prop('checked',true);
-    });
-    
-    //チェックボックスがクリックされた時の動作
-    $("#select label input").click(function() {
-        
-        $(this).parent().toggleClass("selected");
-        
-        $.each($chkbxFilter_tags, function() {
-            if($('#' + this).is(':checked')) {
-                         $("#result " + $chkbxFilter_blocks + ":not(." + this + ")").addClass('hidden-not-' + this);
-                         $chkbxFilter_all.prop('checked',false).parent().removeClass("selected");
-                }
-            else if($('#' + this).not(':checked')) {
-                         $("#result " + $chkbxFilter_blocks + ":not(." + this + ")").removeClass('hidden-not-' + this);
-                }
-        });
-        
-        //チェックボックスが1つも選択されていない場合に、絞り込まないボタンにclass="selected"をつける
-        if ($('.sort:checked').length == 0 ){
-            $chkbxFilter_all.prop('checked',true).parent().addClass("selected");
-            $(".sort").parent().removeClass("selected");
-        };
-	});     
+$(function() {//クリックされたときに実行
+	$(searchItem).on('change', function() {  //最初にサーチアイテム実行で次にサーチフィルタ実行
+		search_filter();
+	});
 });
+
+function search_filter() {
+	$(listItem).removeClass(hideClass); //表示されなくなるクラスをすべてから削除(全て表示状態)
+	
+	for (var i = 0; i < $(searchBox).length; i++) { //クラス名「searchBox」の数だけ繰り返す
+		var name = $(searchBox).eq(i).find('input').attr('name'); //name=サーチボックスの中のインプットの中のname
+		var searchData = selected_items(name);
+		console.log('1回目' + searchData);
+
+		
+		if(searchData.length === 0 || searchData[0] === '') {  //valueが何もなかったら
+			continue;
+		}
+		
+		for (var j = 0; j < $(listItem).length; j++) {  //valueがあったら　一覧の数だけまわすたび　and 一致するもの
+			var itemData = $(listItem).eq(j).data(name); //カテゴリの名前を一つずつitemdataに
+			
+			if(searchData.indexOf(itemData) === -1) {  //一致していないものの行なら非表示クラスに
+				$(listItem).eq(j).addClass(hideClass);
+			}
+			
+			
+	    }  
+    }
+
+	function selected_items(name) {  //チェックされているもののvalueを配列に格納(searchData = [])
+		var searchData = [];
+		$('input[name=' + name + ']:checked').each(function() {
+			searchData.push($(this).val());
+		});
+		console.log('a')
+		return searchData;
+	}
+
+}
--- sort3/sort3.js
+++ sort3/sort3.js
@@ -1,8 +1,9 @@
 
     
+    
     function goFilter2(){
         
-      
+        
             var wTable = document.getElementById("sortTable");
         
         
@@ -37,47 +38,74 @@
             
             var result = str1.split(' ');//その文字列を１単語ずつに分けてresultリストに追加
 
-           console.log('aa' + result[1]);//確認
+           //console.log('aa' + result[1]);//確認
 
            
             var mydiv1 = document.getElementById("id1")//それぞれのタグを一つずつリストに追加
             var result1 = (mydiv1.innerHTML).split(' ');
-            console.log(result1);
+            
 
             var mydiv2 = document.getElementById("id2")//それぞれのタグを一つずつリストに追加
             var result2 = (mydiv2.innerHTML).split(' ');
-            console.log(result2);
+            
+           // console.log(result2);
 
             var mydiv3 = document.getElementById("id3")//それぞれのタグを一つずつリストに追加
             var result3 = (mydiv3.innerHTML).split(' ');
-            console.log(result3);
+           
+           // console.log(result3);
 
             var mydiv4 = document.getElementById("id4")//それぞれのタグを一つずつリストに追加
             var result4 = (mydiv1.innerHTML).split(' ');
-            console.log(result4);
+           // console.log(result4);
 
             var mydiv5 = document.getElementById("id5")//それぞれのタグを一つずつリストに追加
             var result5 = (mydiv5.innerHTML).split(' ');
-            console.log(result5);
+            //console.log(result5);
 
             var mydiv6 = document.getElementById("id6")//それぞれのタグを一つずつリストに追加
             var result6 = (mydiv6.innerHTML).split(' ');
-            console.log(result6);
+            //console.log(result6);
 
             var mydiv7 = document.getElementById("id7")//それぞれのタグを一つずつリストに追加
             var result7 = (mydiv7.innerHTML).split(' ');
-            console.log(result7);
+            //console.log(result7);
 
             var mydiv8 = document.getElementById("id8")//それぞれのタグを一つずつリストに追加
             var result8 = (mydiv8.innerHTML).split(' ');
-            console.log(result8);
+           // console.log(result8);
 
             var mydiv9 = document.getElementById("id9")//それぞれのタグを一つずつリストに追加
             var result9 = (mydiv9.innerHTML).split(' ');
-            console.log(result9);
+           // console.log(result9);
 
-            
+           var resultALL  = ['result1','result2','result3','result4','result5','result6',
+                            'result7','result8','result9']; 
 
+            console.log(resultALL[1]);
+
+
+            var kekka = [];//施設ごとにどれくらい合ってるか
+            var count = 0;//どのくらいあってるかのカウント用
+           
+
+            for(var i=1; result.length - 1 >=i; i++){  //チェックされた要素の数分回す
+                console.log(i+ '回目'); //回った
+
+                
+                for(var y=1; resultALL[i-1].length >= y; y++){ //それぞれの数分
+                    console.log(y+ '回目２'); //回ってない
+
+                    if(result[i] == result+'i'[y]){  //一致してたらカウントに1
+                        count =+ 1;
+                    }
+                    
+                }
+
+                kekka.push(count)
+                console.log('結果：'+ kekka);
+
+            }
 
             //あてはまる文字列が多いほど上に
         }
