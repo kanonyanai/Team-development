d9e63bec454425751dfb035470c4ba42e0287863
te190728 <tejc190728@gmail.com>
2023-01-27 15:36:50 +0900

    ソート機能マージ完了(01/27)

--- home/index_pc.js
+++ home/index_pc.js
@@ -492,31 +492,34 @@ document.getElementById('search').addEventListener('click', initAutocomplete(), 
 
 })
 
-function goFilter2(){
-  var wTable = document.getElementById("sortTable");
-  var value  = '';
+//ソート機能
+function goFilter2() {
+  //　チェックされているチェックボックスの値を取得する
+  let checkedValues = [];
+  const inputs = document.querySelectorAll(".filter-input");
+  for (let input of inputs) {
+    if (input.checked) {
+      checkedValues.push(input.value);
+    }
+  }
 
-  // --- 選択されている商品のクラスを割り当てる ---
-  if(document.getElementById("chkbaby").checked)    {value += ' kbaby2';}
-  if(document.getElementById("chkkuruma").checked){value += ' kuruma2';}
-  if(document.getElementById("chkknyutenok").checked)     {value += ' knyutenok2';}
-  if(document.getElementById("chkksheet").checked)    {value += ' ksheet2';}
-  if(document.getElementById("chkdansang").checked)    {value += ' dansang2';}
-  if(document.getElementById("chkbabyroom").checked)    {value += ' babyroom2';}
-  if(document.getElementById("chkkidsroom").checked)    {value += ' kidsroom2';}
-  if(document.getElementById("chkomutu").checked)    {value += ' omutu2';}
-  if(document.getElementById("chkkyukei").checked)    {value += ' kyukei2';}
-  if(document.getElementById("chktesuri").checked)    {value += ' tesuri2';}
-  if(document.getElementById("chkosuto").checked)    {value += ' osuto2';}
-  if(document.getElementById("chkkyugo").checked)    {value += ' kyugo2';}
-  
+  // テーブルの行を取得する
+  const list = document.querySelectorAll(".table-data");
+  for (let row of list) {
+    // 行のタグを配列として取得
+    const rowTags = row.dataset.tags.split(",");
 
+    let matchTags = [];
+    for (let tag of rowTags) {
+      // チェックされているタグに含まれていれば
+      if (checkedValues.includes(tag)) {
+        matchTags.push(tag);
+      }
+    }
 
-  if(value == ''){
-    // --- 未選択はクラスをクリア ---
-    wTable.className = '';
-  }else{
-    // --- タイトル以外のTRを非表示＋指定属性を持つTRのみ表示 ---
-    wTable.className = 'allNoDisplay2 ' + value;
+    row.style = "";
+    if (checkedValues.length !== matchTags.length) {
+      row.style = "display: none;";
+    }
   }
 }
--- home/index_phone.js
+++ home/index_phone.js
@@ -534,34 +534,37 @@ autocomplete_Stop.bindTo("bounds", map);
 
     return false;
   });
+}
 
-
+//ソート機能
   function goFilter2() {
-    var wTable = document.getElementById("sortTable");
-    var value = '';
-
-    // --- 選択されている商品のクラスを割り当てる ---
-    if (document.getElementById("chkbaby").checked) { value += ' kbaby2'; }
-    if (document.getElementById("chkkuruma").checked) { value += ' kuruma2'; }
-    if (document.getElementById("chkknyutenok").checked) { value += ' knyutenok2'; }
-    if (document.getElementById("chkksheet").checked) { value += ' ksheet2'; }
-    if (document.getElementById("chkdansang").checked) { value += ' dansang2'; }
-    if (document.getElementById("chkbabyroom").checked) { value += ' babyroom2'; }
-    if (document.getElementById("chkkidsroom").checked) { value += ' kidsroom2'; }
-    if (document.getElementById("chkomutu").checked) { value += ' omutu2'; }
-    if (document.getElementById("chkkyukei").checked) { value += ' kyukei2'; }
-    if (document.getElementById("chktesuri").checked) { value += ' tesuri2'; }
-    if (document.getElementById("chkosuto").checked) { value += ' osuto2'; }
-    if (document.getElementById("chkkyugo").checked) { value += ' kyugo2'; }
-
-
-
-    if (value == '') {
-      // --- 未選択はクラスをクリア ---
-      wTable.className = '';
-    } else {
-      // --- タイトル以外のTRを非表示＋指定属性を持つTRのみ表示 ---
-      wTable.className = 'allNoDisplay2 ' + value;
+    //　チェックされているチェックボックスの値を取得する
+    let checkedValues = [];
+    const inputs = document.querySelectorAll(".filter-input");
+    for (let input of inputs) {
+      if (input.checked) {
+        checkedValues.push(input.value);
+      }
     }
-  }
+  
+    // テーブルの行を取得する
+    const list = document.querySelectorAll(".table-data");
+    for (let row of list) {
+      // 行のタグを配列として取得
+      const rowTags = row.dataset.tags.split(",");
+  
+      let matchTags = [];
+      for (let tag of rowTags) {
+        // チェックされているタグに含まれていれば
+        if (checkedValues.includes(tag)) {
+          matchTags.push(tag);
+        }
+      }
+  
+      row.style = "";
+      if (checkedValues.length !== matchTags.length) {
+        row.style = "display: none;";
+      }
+    }
+
 }
